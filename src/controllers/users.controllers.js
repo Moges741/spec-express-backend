9de133eb3c3b026e32e04674ced52189845791c0
Moges741 <mogesse741@gmail.com>
2026-02-20 11:08:53 +0300

    implement to delete

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,10 +1,12 @@
+import { json } from "express";
+
 let users = [];
 let idIncrementor = 0;
 const createUser = (req, res) => {
     try {
         const {name} = req.body;
         if(!name) {
-            res.json({message : "Name is Required"});
+            res.status(404).json({message : "Name is Required"});
         }
         const newUser = {
             id : idIncrementor++,
@@ -39,7 +41,7 @@ const updateUser = (req, res) =>{
     try {
         const user = users.find(u => u.id === Number(req.params.id));
         if(!user){
-            res.json({message : 'User not found'})
+            res.status(404).json({message : 'User not found'})
         }
     user.name = req.body.name || user.name;
     res.json(user)
@@ -54,11 +56,14 @@ const deleteUser = (req, res) => {
     try {
         const index = users.findIndex(u => u.id === Number(req.params.id));
         if(index === -1){
-            res.json({message : 'User not found'})
+            res.status(404).json({message : 'User not found'})
         }
-    
+    const deleted = users.splice(index, 1);
+    res.json(deleted[0])
         
     } catch (error) {
+    res.json({message : error.message})
+
         
     }
 }
